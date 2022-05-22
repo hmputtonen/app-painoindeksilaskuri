2036e98f1f5ac99e279811ca6c55ae9584fe0859
hmputtonen <hmputtonen@gmail.com>
2022-05-22 18:43:14 +0300

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,76 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+describe("Perustoiminnallisuus", () => {
 
-  it(`should have as title 'painoindeksilaskuri'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('painoindeksilaskuri');
-  });
+    const app = new AppComponent();
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('painoindeksilaskuri app is running!');
-  });
+    it("käynnistyy", () => {
+
+        expect(app).toBeTruthy();
+
+    });
+
+    /*
+    it("laskee painoindeksin, kun syöttöarvot ovat oikein", () => {
+
+        app.virhe = "";
+        app.paino = 100;
+        app.pituus = 230;
+        app.painoindeksi = undefined;
+        app.laskePainoindeksi();
+
+
+        expect(app.paino).toBeLessThanOrEqual(230);
+        expect(app.paino).toBeGreaterThanOrEqual(30);
+        expect(app.pituus).toBeLessThanOrEqual(230);
+        expect(app.pituus).toBeGreaterThanOrEqual(100);
+
+        expect(app.painoindeksi).toBeTruthy();
+
+    });
+    
+
+    for(let i = 100 ; i <= 230 ; i++){
+    it(`laskee painoindeksin, kun syötetty pituus on ${i} `, () => {
+
+        app.virhe = "";
+        app.paino = 100;
+        app.pituus= i;
+        app.painoindeksi = undefined;
+        app.laskePainoindeksi();
+
+        expect(app.painoindeksi).toBeTruthy();
+
+
+    });
+}
+
+    for(let i = 30 ; i <= 230 ; i++){
+        it(`laskee painoindeksin, kun syötetty paino on ${i} `, () => {
+
+            app.virhe = "";
+            app.paino = i;
+            app.pituus= 100;
+            app.painoindeksi = undefined;
+            app.laskePainoindeksi();
+
+            expect(app.painoindeksi).toBeTruthy();
+
+
+        });
+    }
+*/
+        it("näyttää virheen epäsopivilla syötteillä", () => {
+
+        app.virhe = "";
+        app.paino = 240;
+        app.pituus = 90;
+        app.laskePainoindeksi();
+        
+        expect(app.painoindeksi).not.toBeTruthy();
+        expect(app.virhe).toContain(`Syöttötiedoissa virhe. Käytä ainoastaan numeerisia arvoja väliltä`);
+
+    });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'painoindeksilaskuri';
+
+  pituus : number;
+  paino : number;
+  painoindeksi : number;
+  virhe : string;
+
+  laskePainoindeksi = () : number => {
+
+
+    if(this.pituus >= 100 && this.pituus <= 230 && this.paino >= 30 && this.paino <= 230){
+
+      
+    let laskettuPainoindeksi = (this.paino/((this.pituus/100)*(this.pituus/100))).toFixed(2);
+    this.painoindeksi = Number(laskettuPainoindeksi);
+    this.painoindeksi;
+  
+    this.virhe = null;
+    return this.painoindeksi;
+      
+
+    } else {
+
+      this.painoindeksi = null;
+      this.virhe = "Syöttötiedoissa virhe. Käytä ainoastaan numeerisia arvoja väliltä 100-230 (pituus) ja 30-230 (paino)";
+    
+    }
+  }
+
 }
